cef69ddd49c4ee1856658803f2451235701dae3e
Alex Karius <2371042+arkari-us@users.noreply.github.com>
2021-05-12 23:49:33 -0500

    Some progress on user creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
 function userController(User) {
   function get(req, res) {
     const query = req.body.payload || {};
@@ -11,8 +14,31 @@ function userController(User) {
     });
   }
 
-  function post(req, res) {
-    const user = new User(req.body.payload);
+  function createUser(req, res) {
+    const data = req.body.payload;
+    const user = new User();
+
+    if (!data.username || !data.password || !data.email) {
+      return res.status(400).send();
+    }
+    user.userName = data.userName;
+    user.email = data.email;
+
+    if (data.password) {
+      bcrypt.hash(data.password, SALT_WORK_FACTOR, function (err, hash) {
+        if (err) {
+          return res.status(500).send();
+        }
+
+        user.password = hash;
+      }
+    }
+    // else if (data.googleToken) { //(TODO)
+    //   user.googleToken = data.googleToken;
+    // }
+    else {
+      return res.status(400).send('No password or third-party token specified');
+    }
 
     // TODO: hash password
     user.save();
@@ -29,7 +55,7 @@ function userController(User) {
     });
   }
 
-  return { post, get, getById };
+  return { createUser, get, getById };
 }
 
 module.exports = userController;
